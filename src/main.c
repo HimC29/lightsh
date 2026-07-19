bc78eb7e304a276c6b7c780b4a1a90a3f981d299
Him C. <himc.29z@gmail.com>
2026-07-19 12:03:57 +0800

    feat: implement command execution via fork/exec/wait with tokenized args

--- src/main.c
+++ src/main.c
@@ -3,11 +3,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/wait.h>
 
 const char* NAME = "lightsh";
-const char* VERSION = "v0.2.0";
+const char* VERSION = "v0.3.0";
 
 #define MAX_LINE 1024
+#define MAX_ARGS 64
 
 struct termios orig_termios;
 
@@ -22,21 +24,19 @@ void disableRawMode() {
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
 
-void enableRawMode() {
+void captureOriginalSettings() {
     if(tcgetattr(STDIN_FILENO, &orig_termios) == -1) {
         perror("Not running in a terminal");
     }
+}
 
-    /* Automatically disable raw mode when the program exits */
-    atexit(disableRawMode);
-
+void enableRawMode() {
     /* Copy the original termios settings and modify it */
     struct termios raw = orig_termios; /* The copying part */
     /* Modifying the setting
      * Clears the bits of ECHO, ICANON, and ISIG, while keeping the rest unchanged. 
      */
     raw.c_lflag &= ~(ECHO | ICANON | ISIG);
-
     /* Apply the raw settings
      * More info about this function inside function disableRawMode
      */
@@ -44,8 +44,11 @@ void enableRawMode() {
 }
 
 int main(void) {
+    /* Automatically disable raw mode when the program exits */
+    atexit(disableRawMode);
+    captureOriginalSettings();
     enableRawMode();
-
+    
     char line[MAX_LINE];
     int len = 0;
     char c;
@@ -54,11 +57,11 @@ int main(void) {
          * Stores the char to c.
          */
         ssize_t nread = read(STDIN_FILENO, &c, 1);
-        /* If input stream has ended */
+        /* If input stream has ended. */
         if (nread == 0) {
             return 0;
         }
-        /* If theres an error in reading */
+        /* If theres an error in reading. */
         else if (nread == -1) {
             perror("Failed to read");
             return 1;
@@ -68,12 +71,49 @@ int main(void) {
         if (c == '\r' || c == '\n') {
             line[len] = '\0';
             len = 0;
-            /* If user types exit */
+            /* Return to column 0 and leave new line. */
+            write(STDOUT_FILENO, "\r\n", 2);
+            /* If user types exit. */
             if (strcmp(line, "exit") == 0) {
                 return 0;
             }
-            /* Return to column 0 and leave new line. */
-            write(STDOUT_FILENO, "\r\n", 2);
+            /* Tokenize the input. */
+            char *args[MAX_ARGS]; /* Stores the arguments. */
+            int i = 0; /* This variable is used to count the amount of times looped. */
+            char *token = strtok(line, " "); /* Set token to the first token. */
+            while (token != NULL) { /* Loop until token ends. */
+               args[i] = token; /* Store the next tokens in order into args. */
+               token = strtok(NULL, " "); /* Set token to the next token. */
+               i++;
+            }
+            /* End args variable in NULL. (execvp uses it to know when it ends) */
+            args[i] = NULL;
+            /* Only fork/exec if there's a command to run */
+            if (args[0] != NULL) {
+                /* Run the command */
+                pid_t pid = fork(); /* Create a twin process. */
+                /* If failed to create a twin process. */
+                if (pid < 0) {
+                   perror("fork");
+                } else if (pid == 0) {
+                   /* Everything in here will be run in the child. */
+                   disableRawMode();
+                   execvp(args[0], args);
+                   /* These runs only if execvp failed to run the command. */
+                   perror("execvp");
+                   exit(1);  
+                } else {
+                    /* Everything in here will be run in the parent. */
+                    int status;
+                    /* Wait until child process finishes. */
+                    waitpid(pid, &status, 0);
+                    /* Enable raw mode again as earlier we disabled it
+                     * Avoids ECHO and write trying to write to the screen at the
+                       same time
+                     */
+                    enableRawMode();
+                }   
+            }
         }
         /* If user presses backspace */
         else if (c == 127 && len > 0) {
@@ -97,6 +137,5 @@ int main(void) {
             write(STDOUT_FILENO, &c, 1);
         }
     }
-
     return 0;
 }
